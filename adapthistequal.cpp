cb406fe36ee8e0be8d73719ca446410b86fffba8
Toby Breckon <toby.breckon@durham.ac.uk>
2017-09-20 04:52:18 +0100

    remove cell checks as resizing fix inside clahe.cpp now removes fixes this

--- adapthistequal.cpp
+++ adapthistequal.cpp
@@ -28,7 +28,7 @@
 
 /******************************************************************************/
 
-// function that takes a gray scale image and draws a histogram 
+// function that takes a gray scale image and draws a histogram
 // image for it in a pre-allocated image
 
 void create_histogram_image(IplImage* grayImg, IplImage* histogramImage){
@@ -38,10 +38,10 @@ void create_histogram_image(IplImage* grayImg, IplImage* histogramImage){
   int hist_size = 256;			// size of histogram (number of bins)
   int bin_w = 0;				// initial width to draw bars
   float range_0[]={0,256};
-  float* ranges[] = { range_0 };	
+  float* ranges[] = { range_0 };
 
   hist = cvCreateHist(1, &hist_size, CV_HIST_ARRAY, ranges, 1);
-  
+
   cvCalcHist( &grayImg, hist, 0, NULL );
   cvGetMinMaxHistValue( hist, 0, &max_value, 0, 0 );
   cvScale( hist->bins, hist->bins, ((double)histogramImage->height)/max_value, 0 );
@@ -51,161 +51,150 @@ void create_histogram_image(IplImage* grayImg, IplImage* histogramImage){
   for(int i = 0; i < hist_size; i++ )
   {
      cvRectangle( histogramImage, cvPoint(i*bin_w, histogramImage->height),
-                  cvPoint((i+1)*bin_w, histogramImage->height 
+                  cvPoint((i+1)*bin_w, histogramImage->height
 	  								- cvRound(cvGetReal1D(hist->bins,i))),
                    					cvScalarAll(0), -1, 8, 0 );
   }
-  
+
   cvReleaseHist (&hist);
-}	
+}
 
 /******************************************************************************/
 
 int main( int argc, char** argv )
 {
 
-  IplImage* img = NULL;      // image object 
+  IplImage* img = NULL;      // image object
   CvCapture* capture = NULL; // capture object
-	
+
   IplImage* eqHistogramImage = NULL;  	// histogram images
-  IplImage* grayHistogramImage = NULL;	
-	
+  IplImage* grayHistogramImage = NULL;
+
   const char* windowName = "CL Adaptive Histogram Equalization"; // window name
   const char* windowName1 = "Grayscale"; // window name
   const char* windowNameH1 = "Adaptive Equalised Histogram"; // window name
   const char* windowNameH2 = "Original Histogram"; // window name
-	
+
   bool keepProcessing = true;	// loop control flag
-  char key;						// user input	
+  char key;						// user input
   int  EVENT_LOOP_DELAY = 40;	// delay for GUI window
-                                // 40 ms equates to 1000ms/25fps = 40ms per frame		
-	
+                                // 40 ms equates to 1000ms/25fps = 40ms per frame
+
   int xdivs = 2;
   int ydivs = 2;
   int bins = 256;
-  int limit_counter = 14;	
-	
-  // if command line arguments are provided try to read image/video_name
-  // otherwise default to capture from attached H/W camera 
+  int limit_counter = 14;
 
-    if( 
+  // if command line arguments are provided try to read image/video_name
+  // otherwise default to capture from attached H/W camera
+
+    if(
 	  ( argc == 2 && (img = cvLoadImage( argv[1], 1)) != 0 ) ||
-	  ( argc == 2 && (capture = cvCreateFileCapture( argv[1] )) != 0 ) || 
+	  ( argc == 2 && (capture = cvCreateFileCapture( argv[1] )) != 0 ) ||
 	  ( argc != 2 && (capture = cvCreateCameraCapture( 0 )) != 0 )
 	  )
     {
       // create window object (use flag=0 to allow resize, 1 to auto fix size)
 
-      cvNamedWindow(windowName, 1);		// flag set to 1 by Shervin Emami, 17Nov2010.
+    cvNamedWindow(windowName, 1);		// flag set to 1 by Shervin Emami, 17Nov2010.
 	  cvNamedWindow(windowName1, 1);	// flag set to 1 by Shervin Emami, 17Nov2010.
 	  cvNamedWindow(windowNameH1, 1);	// flag set to 1 by Shervin Emami, 17Nov2010.
 	  cvNamedWindow(windowNameH2, 1);	// flag set to 1 by Shervin Emami, 17Nov2010.
-		
-      cvNamedWindow("Simple Histogram Equalization", 1);		// Added by Shervin Emami, 17Nov2010.
 
-	  cvCreateTrackbar("X cells", windowName, &xdivs, 16, NULL);				
+    cvNamedWindow("Simple Histogram Equalization", 1);		// Added by Shervin Emami, 17Nov2010.
+
+	  cvCreateTrackbar("X cells", windowName, &xdivs, 16, NULL);
 	  cvCreateTrackbar("Y cells", windowName, &ydivs, 16, NULL);
 	  cvCreateTrackbar("bins", windowName, &bins, 256, NULL);
 	  cvCreateTrackbar("limit (x 0.1)", windowName, &limit_counter, 30, NULL);
-		
+
 	  // define required images for intermediate processing
-      // (if using a capture object we need to get a frame first to get the size)
-		
+    // (if using a capture object we need to get a frame first to get the size)
+
 	  if (capture) {
-	
-		  // cvQueryFrame s just a combination of cvGrabFrame 
+
+		  // cvQueryFrame s just a combination of cvGrabFrame
 		  // and cvRetrieveFrame in one call.
-		  
+
 		  img = cvQueryFrame(capture);
 		  if(!img){
-			if (argc == 2){				  
+			if (argc == 2){
 				printf("End of video file reached\n");
 			} else {
 				printf("ERROR: cannot get next fram from camera\n");
 			}
 			exit(0);
 		  }
-		  
+
 	  }
-	  
-	  IplImage* grayImg = 
+
+	  IplImage* grayImg =
 	  			cvCreateImage(cvSize(img->width,img->height), img->depth, 1);
 	  grayImg->origin = img->origin;
-	  IplImage* eqImg = 
+	  IplImage* eqImg =
 	  			cvCreateImage(cvSize(img->width,img->height), img->depth, 1);
-	  eqImg->origin = img->origin;		
-		
+	  eqImg->origin = img->origin;
+
 	  eqHistogramImage = cvCreateImage(cvSize(255,200), 8, 1);
 	  grayHistogramImage = cvCreateImage(cvSize(255,200), 8, 1);
-	  
-	  // start main loop	
-		
+
+	  // start main loop
+
 	  while (keepProcessing) {
-		
+
 		  // if capture object in use (i.e. video/camera)
 		  // get image from capture object
-			
+
 		  if (capture) {
-	
-			  // cvQueryFrame s just a combination of cvGrabFrame 
+
+			  // cvQueryFrame s just a combination of cvGrabFrame
 			  // and cvRetrieveFrame in one call.
-			  
+
 			  img = cvQueryFrame(capture);
 			  if(!img){
-				if (argc == 2){				  
-					printf("End of video file reached\n");
-				} else {
-					printf("ERROR: cannot get next fram from camera\n");
-				}
-				exit(0);
+					if (argc == 2){
+						printf("End of video file reached\n");
+					} else {
+						printf("ERROR: cannot get next fram from camera\n");
+					}
+					exit(0);
 			  }
-			  
+
 		  }	else {
-			  
+
 			  // if not a capture object set event delay to zero so it waits
 			  // indefinitely (as single image file, no need to loop)
-			  
+
 			  EVENT_LOOP_DELAY = 0;
-		  }			  
-		
+		  }
+
 		  // *** Histogram Equalisation Processing
-		  
+
 			  // if input is not already grayscale, convert to grayscale
-			
+
 			  if (img->nChannels > 1){
 				  cvCvtColor(img, grayImg, CV_BGR2GRAY);
 			  } else {
 				  grayImg = img;
 			  }
-		   
-			  // histogram equalize it (checking first for valid cells numbers)
-//			  if ((img->width % xdivs) || (img->height % ydivs))
-			  if (0)	// Check has been removed by Shervin Emami, 17Nov2010.
-			  {
-				printf("X cells and Y cells must be multiples of image height and image width\n\n");
 
-			  } else {
-  			    CV_TIMER_START(X)  
- 				cvCLAdaptEqualize(grayImg, eqImg, (unsigned int) xdivs, (unsigned int) ydivs, 
+  			CV_TIMER_START(X)
+ 					cvCLAdaptEqualize(grayImg, eqImg, (unsigned int) xdivs, (unsigned int) ydivs,
 					(unsigned int) bins, (float) limit_counter * 0.1, CV_CLAHE_RANGE_FULL);
-			  	CV_TIMER_STOP(X, "CLAHE")
-			  }
-			  
-			  
-		  // ***
-		  
-	      // *** draw histograms 	  
-			
+			  CV_TIMER_STOP(X, "CLAHE")
+
+	    // *** draw histograms
+
 		  create_histogram_image(grayImg, grayHistogramImage);
-		  create_histogram_image(eqImg, eqHistogramImage);		  
-			  
+		  create_histogram_image(eqImg, eqHistogramImage);
+
 		  // display image in window
-	
+
 		  cvShowImage( windowName,  eqImg );
 		  cvShowImage( windowName1, grayImg );
-		  
+
 		  cvShowImage( windowNameH1,  eqHistogramImage );
-		  cvShowImage( windowNameH2, grayHistogramImage );	  
+		  cvShowImage( windowNameH2, grayHistogramImage );
 
 		  // Simple Histogram Equalization. Added by Shervin Emami, 17Nov2010.
 		  IplImage *histEqImage = cvCreateImage(cvGetSize(grayImg), grayImg->depth, grayImg->nChannels);
@@ -215,43 +204,43 @@ int main( int argc, char** argv )
 
 		  // start event processing loop (very important,in fact essential for GUI)
 	      // 4 ms roughly equates to 100ms/25fps = 4ms per frame
-		  
+
 		  key = cvWaitKey(EVENT_LOOP_DELAY);
 
 		  if (key == 'x' || key == 27) {	// 'Esc' key added by Shervin Emami, 17Nov2010.
-			
+
 	   		// if user presses "x" or 'Esc' then exit
-			
-	   			printf("Keyboard exit requested : exiting now - bye!\n");	
+
+	   			printf("Keyboard exit requested : exiting now - bye!\n");
 	   			keepProcessing = false;
-		  } 
+		  }
 	  }
-      
-      // destroy window objects
-      // (triggered by event loop *only* window is closed)
 
-      cvDestroyAllWindows();
+    // destroy window objects
+    // (triggered by event loop *only* window is closed)
 
-      // destroy image object (if it does not originate from a capture object)
+    cvDestroyAllWindows();
 
-      if (!capture){
-		  cvReleaseImage( &img );
-      }
+    // destroy image object (if it does not originate from a capture object)
+
+    if (!capture){
+			cvReleaseImage( &img );
+    }
 	  cvReleaseImage( &grayImg );
 	  cvReleaseImage( &eqImg );
-	  
+
 	  // release histogram images
-		  
+
 	  cvReleaseImage( &eqHistogramImage );
-      cvReleaseImage( &grayHistogramImage );
-	  
-      // all OK : main returns 0
+    cvReleaseImage( &grayHistogramImage );
 
-      return 0;
-    }
+    // all OK : main returns 0
 
-    // not OK : main returns -1
+    return 0;
+  }
 
-    return -1;
+  // not OK : main returns -1
+
+  return -1;
 }
 /******************************************************************************/
